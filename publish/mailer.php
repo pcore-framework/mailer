1b84a9b952a8fdabbbfab512932cf61a7cad0c8f
Magomedcoder <info@magomedcoder.ru>
2022-07-30 02:36:20 +0300

    Added config provider

---
+++ publish/mailer.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'smtp' => [
+        'host' => 'localhost',
+        'port' => 465,
+        'encryption' => 'ssl',
+        'username' => '',
+        'password' => ''
+    ]
+];
